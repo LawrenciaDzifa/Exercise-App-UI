02b2e42277714fe57bc194ce817ce00a84cc92e0
Lawrencia Dzifa <kweku01bright@gmail.com>
2020-11-04 11:34:22 +0000

    first commit

--- lib/Components/activities_bank.dart
+++ lib/Components/activities_bank.dart
@@ -4,30 +4,26 @@ int activityNumber = 0;
 class Activity {
   String activityName;
   String image;
-  int activityNumber;
+  
 
-  Activity({ this.activityName,  this.image, this.activityNumber});
+  Activity({ this.activityName,  this.image, });
 
   List<Activity> activityBank = [
     Activity(
       activityName: 'Diet Management',
       image: 'assets/images/diet3.jpg',
-      activityNumber: 0,
     ),
     Activity(
       activityName: 'Kegel ',
       image: 'assets/images/workout.jpg',
-      activityNumber: 1,
     ),
     Activity(
       activityName: 'Yoga ',
       image: 'assets/images/yoga.jpg',
-      activityNumber: 2,
     ),
     Activity(
       activityName: 'Meditation ',
       image: 'assets/images/meditation1.jpg',
-      activityNumber: 3,
     ),
   ];
 }
