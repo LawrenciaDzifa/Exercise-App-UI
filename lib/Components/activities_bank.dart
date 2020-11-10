744d95a959a748f461a2306fd501ef39a24238a8
Williano <paawilly17@gmail.com>
2020-11-09 23:08:28 -0600

    Add functionality to display activity name

--- lib/Components/activities_bank.dart
+++ lib/Components/activities_bank.dart
@@ -1,32 +1,35 @@
 int activityNumber = 0;
-  var activityBank;
+var activityBank;
 
 class Activity {
   String activityName;
   String image;
-  
 
-  Activity({ this.activityName,  this.image, });
-
-  List<Activity> activityBank = [
-    Activity(
-      activityName: 'Diet Management',
-      image: 'assets/images/diet3.jpg',
-    ),
-    Activity(
-      activityName: 'Kegel ',
-      image: 'assets/images/workout.jpg',
-    ),
-    Activity(
-      activityName: 'Yoga ',
-      image: 'assets/images/yoga.jpg',
-    ),
-    Activity(
-      activityName: 'Meditation ',
-      image: 'assets/images/meditation1.jpg',
-    ),
-  ];
+  Activity({
+    this.activityName,
+    this.image,
+  });
 }
-getActivityName(activityNumber){
-    return activityBank[activityNumber].activityName;
+
+List<Activity> getAllActivities() => [
+      Activity(
+        activityName: 'Diet Management',
+        image: 'assets/images/diet3.jpg',
+      ),
+      Activity(
+        activityName: 'Kegel ',
+        image: 'assets/images/workout.jpg',
+      ),
+      Activity(
+        activityName: 'Yoga ',
+        image: 'assets/images/yoga.jpg',
+      ),
+      Activity(
+        activityName: 'Meditation ',
+        image: 'assets/images/meditation1.jpg',
+      ),
+    ];
+
+getActivityName(activityNumber) {
+  return activityBank[activityNumber].activityName;
 }
